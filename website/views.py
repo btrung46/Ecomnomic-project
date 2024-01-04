10069fb46e333756df042e32843a064618f4433a
bTrung <nguyenviethoang040603@gmail.com>
2024-01-04 22:05:38 +0700

    build model

--- website/views.py
+++ website/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    return render(request,'home.html',{})
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['username']
+        user = authenticate(request, username= username,password= password)
+        if user is not None:
+            pass
+    return render(request, 'login.html',{})
+def logout_user(request):
+    pass 
